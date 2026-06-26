4250577243cd9d55e210ec3c8cd1d680362b05bc
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-26 17:35:14 -0400

    Update GestorReclamos.java

--- GestorReclamos.java
+++ GestorReclamos.java
@@ -1,1 +1,49 @@
+package reclamosmunicipales;
 
+public class GestorReclamos {
+
+    private ListaReclamos listaReclamos;
+
+    public GestorReclamos() {
+        this.listaReclamos = new ListaReclamos();
+    }
+
+    public void registrarReclamo(Reclamo reclamo) {
+        listaReclamos.agregar(reclamo);
+        System.out.println("Reclamo registrado correctamente.");
+    }
+
+    public boolean modificarDescripcion(int codigo, String nuevaDescripcion) {
+        Reclamo[] arr = listaReclamos.aArreglo();
+        for (Reclamo r : arr) {
+            if (r.getCodigo() == codigo) {
+                r.setDescripcion(nuevaDescripcion);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean actualizarEstado(int codigo, String nuevoEstado) {
+        Reclamo[] arr = listaReclamos.aArreglo();
+        for (Reclamo r : arr) {
+            if (r.getCodigo() == codigo) {
+                r.setEstado(nuevoEstado);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean eliminarReclamo(int codigo) {
+        return listaReclamos.eliminar(codigo);
+    }
+
+    public void mostrarTodos() {
+        listaReclamos.mostrarTodos();
+    }
+
+    public int cantidadReclamos() {
+        return listaReclamos.getCantidad();
+    }
+}
