ae0406de11477c19f0e90152708fdf49d7507214
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-26 17:37:35 -0400

    Update PilaHistorial.java

--- PilaHistorial.java
+++ PilaHistorial.java
@@ -1,1 +1,36 @@
+package reclamosmunicipales;
 
+public class PilaHistorial {
+
+    private class NodoHistorial {
+        String cambio;
+        NodoHistorial siguiente;
+
+        NodoHistorial(String cambio) {
+            this.cambio = cambio;
+            this.siguiente = null;
+        }
+    }
+
+    private NodoHistorial tope;
+    private int cantidad;
+
+    public PilaHistorial() {
+        this.tope = null;
+        this.cantidad = 0;
+    }
+
+    public void apilar(String cambio) {
+        NodoHistorial nuevo = new NodoHistorial(cambio);
+        nuevo.siguiente = tope;
+        tope = nuevo;
+        cantidad++;
+    }
+
+    public String desapilar() {
+        if (tope == null) return null;
+        String cambio = tope.cambio;
+        tope = tope.siguiente;
+        cantidad--;
+        return cambio;
+    }
