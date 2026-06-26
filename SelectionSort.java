992e3f5ebc29e9fb49e3aaa4becd8aedec33dec5
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-26 17:38:31 -0400

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,1 +1,9 @@
+package reclamosmunicipales;
 
+public class SelectionSort {
+
+    public static void ordenarPorPrioridad(Reclamo[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int indiceMin = i;
+            for (int j = i + 1; j < n; j++) {
