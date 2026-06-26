e6200473f7a1efa4a5db9eae1b99505cf851c899
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-26 17:36:43 -0400

    Add ColaReclamos class for managing complaints

--- ColaReclamos.java
+++ ColaReclamos.java
@@ -1,1 +1,23 @@
+package reclamosmunicipales;
 
+public class ColaReclamos {
+
+    private class NodoCola {
+        Reclamo reclamo;
+        NodoCola siguiente;
+
+        NodoCola(Reclamo reclamo) {
+            this.reclamo = reclamo;
+            this.siguiente = null;
+        }
+    }
+
+    private NodoCola frente;
+    private NodoCola fondo;
+    private int cantidad;
+
+    public ColaReclamos() {
+        this.frente = null;
+        this.fondo = null;
+        this.cantidad = 0;
+    }
