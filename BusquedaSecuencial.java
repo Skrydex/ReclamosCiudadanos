7e18c5c72179544f14b246135558cbb414742c95
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-26 17:48:36 -0400

    Update BusquedaSecuencial.java

--- BusquedaSecuencial.java
+++ BusquedaSecuencial.java
@@ -1,1 +1,18 @@
+package reclamosmunicipales;
 
+/**
+ *
+ * @author basti
+ */
+
+public class BusquedaSecuencial {
+
+    public static Reclamo buscarPorCodigo(Reclamo[] arr, int codigo) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getCodigo() == codigo) {
+                return arr[i];
+            }
+        }
+        return null;
+    }
+}
