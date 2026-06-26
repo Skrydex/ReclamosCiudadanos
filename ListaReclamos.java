5758c8b84e996c2431f790fcb4babb39469a4bc8
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-26 17:15:04 -0400

    Update ListaReclamos.java

--- ListaReclamos.java
+++ ListaReclamos.java
@@ -1,1 +1,100 @@
+package reclamosmunicipales;
 
+/**
+ *
+ * @author basti
+ */
+public class ListaReclamos {
+
+    // clase interna del nodo
+    private class NodoReclamo {
+        Reclamo reclamo;
+        NodoReclamo siguiente;
+
+        NodoReclamo(Reclamo reclamo){
+            this.reclamo = reclamo;
+            this.siguiente = null;
+        }
+    }
+
+    private NodoReclamo cabeza;
+    private int cantidad;
+
+    public ListaReclamos(){
+        this.cabeza = null;
+        this.cantidad = 0;
+    }
+
+    // insercion al final
+    public void agregar(Reclamo reclamo){
+        NodoReclamo nuevo = new NodoReclamo(reclamo);
+
+        if(cabeza == null){
+            cabeza = nuevo;
+        } else {
+            NodoReclamo actual = cabeza;
+            while(actual.siguiente != null){
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+        cantidad++;
+    }
+
+    // eliminacion por codigo
+    public boolean eliminar(int codigo){
+        if(cabeza == null){
+            return false;
+        }
+
+        if(cabeza.reclamo.getCodigo() == codigo){
+            cabeza = cabeza.siguiente;
+            cantidad--;
+            return true;
+        }
+
+        NodoReclamo actual = cabeza;
+        while(actual.siguiente != null){
+            if(actual.siguiente.reclamo.getCodigo() == codigo){
+                actual.siguiente = actual.siguiente.siguiente;
+                cantidad--;
+                return true;
+            }
+            actual = actual.siguiente;
+        }
+
+        return false;
+    }
+
+    // convierte la lista enlazada a arreglo, para poder ordenar y buscar facilmente cualqueir reclamo
+    public Reclamo[] aArreglo(){
+        Reclamo[] arreglo = new Reclamo[cantidad];
+        NodoReclamo actual = cabeza;
+        int i = 0;
+
+        while(actual != null){
+            arreglo[i] = actual.reclamo;
+            actual = actual.siguiente;
+            i++;
+        }
+
+        return arreglo;
+    }
+
+    public void mostrarTodos(){
+        if(cabeza == null){
+            System.out.println("No hay reclamos registrados.");
+            return;
+        }
+
+        NodoReclamo actual = cabeza;
+        while(actual != null){
+            System.out.println(actual.reclamo);
+            actual = actual.siguiente;
+        }
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+}
