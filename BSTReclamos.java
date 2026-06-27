5007a6761f45d02d58db25b48360e81af568848e
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-27 14:38:44 -0400

    Add BSTReclamos class with nested NodoBST

--- BSTReclamos.java
+++ BSTReclamos.java
@@ -1,1 +1,15 @@
+package reclamosmunicipales;
 
+public class BSTReclamos {
+
+    private class NodoBST {
+        Reclamo reclamo;
+        NodoBST izquierdo;
+        NodoBST derecho;
+
+        NodoBST(Reclamo reclamo) {
+            this.reclamo = reclamo;
+            this.izquierdo = null;
+            this.derecho = null;
+        }
+    }
