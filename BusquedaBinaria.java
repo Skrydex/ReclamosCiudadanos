eae29a575a0b9f23366123da30d6fcafdc8e4b52
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-27 14:36:14 -0400

    Update BusquedaBinaria.java

--- BusquedaBinaria.java
+++ BusquedaBinaria.java
@@ -1,1 +1,8 @@
+package reclamosmunicipales;
 
+public class BusquedaBinaria {
+
+    public static Reclamo buscarPorCodigo(Reclamo[] arr, int codigo, int izquierda, int derecha) {
+        if (izquierda > derecha) return null;
+
+        int medio = (izquierda + derecha) / 2;
