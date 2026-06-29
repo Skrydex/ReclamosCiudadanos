bd1846f9d362d549cfcfbc74ac23257d1552956d
Agentem1302k <91393651+Agentem1302k@users.noreply.github.com>
2026-06-29 17:46:39 -0400

    Implement selection sort for Reclamo objects

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,9 +1,18 @@
 package reclamosmunicipales;
 
 public class SelectionSort {
-
     public static void ordenarPorPrioridad(Reclamo[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             int indiceMin = i;
             for (int j = i + 1; j < n; j++) {
+                if(arr[j].getPrioridad() < arr[indiceMin].getPrioridad()){
+                    indiceMin = j;
+                }
+            }
+            Reclamo temp = arr[i];
+            arr[i] = arr[indiceMin];
+            arr[indiceMin] = temp;
+        }
+    }
+}
