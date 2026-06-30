23bd4befebb95414fee5c03ffa2d876c685031b5
Skrydex <164804836+Skrydex@users.noreply.github.com>
2026-06-29 20:16:50 -0400

    Update ColaReclamos.java

--- ColaReclamos.java
+++ ColaReclamos.java
@@ -1,12 +1,16 @@
 package reclamosmunicipales;
 
+/**
+ *
+ * @author basti
+ */
 public class ColaReclamos {
 
     private class NodoCola {
         Reclamo reclamo;
         NodoCola siguiente;
 
-        NodoCola(Reclamo reclamo) {
+        NodoCola(Reclamo reclamo){
             this.reclamo = reclamo;
             this.siguiente = null;
         }
@@ -16,8 +20,63 @@ public class ColaReclamos {
     private NodoCola fondo;
     private int cantidad;
 
-    public ColaReclamos() {
+    public ColaReclamos(){
         this.frente = null;
         this.fondo = null;
         this.cantidad = 0;
     }
+
+    // encolamiento
+    public void encolar(Reclamo reclamo){
+        NodoCola nuevo = new NodoCola(reclamo);
+
+        if(frente == null){
+            frente = nuevo;
+            fondo = nuevo;
+        } else {
+            fondo.siguiente = nuevo;
+            fondo = nuevo;
+        }
+        cantidad++;
+    }
+
+    // desencolamiento
+    public Reclamo desencolar(){
+        if(frente == null){
+            return null;
+        }
+
+        Reclamo reclamo = frente.reclamo;
+        frente = frente.siguiente;
+
+        if(frente == null){
+            fondo = null;
+        }
+
+        cantidad--;
+        return reclamo;
+    }
+
+    public boolean estaVacia(){
+        return frente == null;
+    }
+
+    public void mostrarPendientes(){
+        if(frente == null){
+            System.out.println("No hay reclamos pendientes en la cola.");
+            return;
+        }
+
+        NodoCola actual = frente;
+        int posicion = 1;
+        while(actual != null){
+            System.out.println(posicion + ". " + actual.reclamo);
+            actual = actual.siguiente;
+            posicion++;
+        }
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+}
